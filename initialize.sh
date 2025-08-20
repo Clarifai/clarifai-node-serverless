83c1755e0a7cfd12ab0572c9971e042d7a31e07d
DaniAkash <dani@clarifai.com>
2025-08-20 14:41:35 +0530

    chore: update initializer script

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,12 @@
 # Fetch latest clarifai proto files
-npx degit clarifai/clarifai-nodejs-grpc/proto proto/
+npx degit --force clarifai/clarifai-nodejs-grpc/proto proto/
 
 # Delete unnecessary files
 rm proto/clarifai/api/service_processed.proto
 find ./proto -type f ! -name '*.proto' -delete
 
 # Fetch latest google proto files
-npx degit gogo/protobuf/protobuf/google/ google/
-npx degit googleapis/googleapis/google/api/ google/api/
+npx degit --force gogo/protobuf/protobuf/google/ google/
+npx degit --force googleapis/googleapis/google/api/ google/api/
 
 protoc --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts_proto --ts_out=./src/generated --proto_path=. $(find proto/clarifai -name '*.proto')
